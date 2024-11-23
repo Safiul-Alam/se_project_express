5e2f6dddf925c42c78faa7d036ce92c5d293dcd3
Safiul alam, Mohammad <alam.buff@gmail.com>
2024-11-22 19:57:27 -0500

    .

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -40,4 +40,133 @@ const updateItem = (req, res) => {
     );
 };
 
-module.exports = { createItem, getItems, updateItem };
+
+const mongoose = require("mongoose");
+const ClothingItem = require('../models/clothingItem');
+
+const createItem = (req, res) => {
+  console.log(req);
+  console.log(req.body);
+  console.log(req.user._id);
+
+  const { name, weather, imageUrl } = req.body;
+  ClothingItem.create({ name, weather, imageUrl, owner: req.user._id })
+    .then((item) => {
+      res.status(200).send({ item });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: err.message });
+      }
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+const getItems = (req, res) => {
+  ClothingItem.find().then((items) => res.status(200).send((items)))
+  .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        res.status(400).send({ message: err.message });
+      }
+    res.status(500).send({message:"Error from getItems", err})
+  })
+};
+
+const likeItem = (req, res) => {
+  const { itemId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(itemId)) {
+    return res.status(400).send({ message: "Invalid ID format" });
+  }
+
+  return ClothingItem.findByIdAndUpdate(
+    itemId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+    .then((item) => {
+      if (!item) {
+        return res.status(404).send({ message: "Item not found" });
+      }
+      return res.status(200).send({ data: item });
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        return res.status(400).send({ message: "Item not found" });
+      }
+      return res
+        .status(500)
+        .send({ message: "Internal server error" });
+    });
+};
+
+const unlikeItem = (req, res) => {
+  const { itemId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(itemId)) {
+    return res.status(400).send({ message: "Invalid ID format" });
+  }
+
+  return ClothingItem.findByIdAndUpdate(
+    itemId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail(() => {
+      const error = new Error("Card ID not found");
+      error.statusCode = 404;
+      throw error;
+    })
+    .then((item) => {
+      if (!item) {
+        return res.status(404).send({ message: "Item not found" });
+      }
+      return res.status(200).send({ data: item });
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        return res.status(400).send({ message: "Item not found" });
+      }
+      return res
+        .status(404)
+        .send({ message: "Internal server error" });
+    });
+};
+
+const updateItem = (req, res) => {
+  const {itemId} = req.params;
+  const {imageURL} = req.body;
+
+  ClothingItem.findByIdAndUpdate(itemId, {$set: {imageURL}}).orFail().then((item) => res.status(200).send({data:item}))
+  .catch((e) => {
+     res.status(500).send({message:"Error from updateItem", e})})
+}
+
+const deleteItem = (req, res) => {
+  const { itemId } = req.params;
+
+  console.log(itemId);
+  ClothingItem.findByIdAndDelete(itemId)
+    .orFail(() => {
+      const error = new Error("User ID not found");
+      error.name = "DocumentNotFoundError";
+      throw error;
+    })
+    .then(() => res.status(200).send({ message: "Deletion successful" })).catch((err) => {
+      console.error(err);
+      if (err.name === "CastError") {
+        return res.status(400).send({ message: err.message });
+      }
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = { createItem, getItems, updateItem, deleteItem };
--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,15 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { createItem, getItems, updateItem } = require('../controllers/clothingItems');
+const {
+  createItem,
+  getItems,
+  updateItem,
+  deleteItem,
+} = require("../controllers/clothingItems");
 
 // Create
-router.post('/', createItem);
+router.post("/", createItem);
 
 // READ
-router.get('/', getItems);
+router.get("/", getItems);
 
 // UPdate
-router.put('/:itemId', updateItem);
+router.put("/:itemId", updateItem);
+
+// Delete
+router.delete("/:itemId", deleteItem);
 
 module.exports = router;
