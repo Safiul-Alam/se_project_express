3c4ada5e762ee4f8a03a42ad398298ce004fb08a
Safiul alam, Mohammad <alam.buff@gmail.com>
2024-11-23 13:28:57 -0500

    .

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -16,7 +16,7 @@ const getItems = (req, res) => {
   ClothingItem.find()
     .then((items) => res.status(200).send(items))
     .catch((err) => {
-      console.log(err);
+      // console.log(err);
       if (err.name === "ValidationError") {
         res.status(400).send({ message: err.message });
       }
@@ -25,9 +25,9 @@ const getItems = (req, res) => {
 };
 
 const createItem = (req, res) => {
-  console.log(req);
-  console.log(req.body);
-  console.log(req.user._id);
+  // console.log(req);
+  // console.log(req.body);
+  // console.log(req.user._id);
 
   const { name, weather, imageUrl } = req.body;
   ClothingItem.create({ name, weather, imageUrl, owner: req.user._id })
@@ -67,7 +67,7 @@ const updateItem = (req, res) => {
     .then((item) => res.status(200).send({ data: item }))
     .catch((err) =>{
       console.error(err);
-      console.log(err.name);
+      // console.log(err.name);
       res.status(500).send({ message: "Error from updateItem", err })
     }
 
@@ -78,7 +78,7 @@ const updateItem = (req, res) => {
 const deleteItem = (req, res) => {
   const { itemId } = req.params;
 
-  console.log(itemId);
+  // console.log(itemId);
   ClothingItem.findByIdAndDelete(itemId)
     .orFail(() => {
       const error = new Error("User ID not found");
