b67b8dd4063bf0779523ba868a31fe5096b3c167
Safiul alam, Mohammad <alam.buff@gmail.com>
2024-11-24 17:03:42 -0500

    .

--- utils/errors.js
+++ utils/errors.js
@@ -1,5 +1,3 @@
-
-
 module.exports = {
   castErrorMessage: "Invalid data format",
   validationErrorMessage: "Data failed validation",
@@ -15,18 +13,23 @@ module.exports = {
     "Request limit exceeded, please wait before trying again",
 };
 
-
 const {
-  STATUS_BAD_REQUEST, ,
-  STATUS_NOT_FOUND, STATUS_DEFAULT} = require('./constants');
+  STATUS_BAD_REQUEST,
+  STATUS_NOT_FOUND,
+  STATUS_DEFAULT,
+} = require("./constants");
 
 const handleErrors = (res, err) => {
   console.error(err);
   if (err.name === "ValidationError" || err.name === "CastError") {
-    return res.status(STATUS_BAD_REQUEST).send({ success: false, message: err.message });
+    return res
+      .status(STATUS_BAD_REQUEST)
+      .send({ success: false, message: err.message });
   }
   if (err.name === "DocumentNotFoundError") {
-    return res.status(STATUS_NOT_FOUND).send({ success: false, message: err.message });
+    return res
+      .status(STATUS_NOT_FOUND)
+      .send({ success: false, message: err.message });
   }
   return res
     .status(STATUS_DEFAULT)
