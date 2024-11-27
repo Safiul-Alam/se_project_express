30972fd8b7aaef0b2b8b2357e0bea6f7e6256e56
Safiul alam, Mohammad <alam.buff@gmail.com>
2024-11-27 11:47:30 -0500

    .

--- controllers/users.js
+++ controllers/users.js
@@ -10,6 +10,7 @@ const {
   STATUS_BAD_REQUEST,
   STATUS_DEFAULT,
   STATUS_NOT_FOUND,
+  STATUS_CREATED
 } = require("../utils/constants");
 const handleErrors = require("../utils/errors");
 const { JWT_SECRET } = require("../utils/constants");
@@ -56,51 +57,66 @@ const getUser = (req, res) => {
 //       handleErrors(res, err);
 //     });
 // };
-const createUsers = (req, res, next) => {
-  const { email, password } = req.body;
 
-  // Check if email and password are provided
+const createUsers = (req, res) => {
+  const { name, avatar, email, password } = req.body;
+
+  // Validate required fields
   if (!email || !password) {
     return res
       .status(STATUS_BAD_REQUEST)
-      .send({ message: "Email and password are required." });
+      .send({ message: "The email and password fields are required." });
   }
 
-  // Find the user by email
-  User.findOne({ email: email.toLowerCase() })
-    .select("+password")
+  // Check if the user already exists
+  User.findOne({ email })
     .then((user) => {
-      if (!user) {
-        // If no user found with the email
-        return res
-          .status(STATUS_UNAUTHORIZE)
-          .send({ message: "Invalid email or password." });
+      if (user) {
+        const error = new Error("The user already exists");
+        error.statusCode = STATUS_UNAUTHORIZE;
+        throw error;
       }
 
-      // Validate the password
-      return bcrypt.compare(password, user.password).then((isMatch) => {
-        if (!isMatch) {
-          // If the password is incorrect
-          return res
-            .status(401)
-            .send({ message: "Invalid email or password." });
-        }
-
-        // Generate a token
-        const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
-          expiresIn: "7d",
-        });
-
-        // Respond with user data and token
-        res.send({
-          token,
-          name: user.name,
-          avatar: user.avatar,
-          email: user.email,
-        });
+      // Hash the password
+      return bcrypt.hash(password, 10);
+    })
+    .then((hash) => {
+      // Create the user with the hashed password
+      User.create({
+        name,
+        avatar,
+        email,
+        password: hash,
       });
     })
-    .catch(next); // Handle unexpected errors
+    .then((user) => {
+      // Remove the password from the returned object
+      const userWithoutPassword = user.toObject();
+      delete userWithoutPassword.password;
+
+      // Respond with the created user
+      res.status(STATUS_CREATED).json(userWithoutPassword);
+    })
+    .catch((err) => {
+      console.error(err);
+
+      // Handle validation errors
+      if (err.name === "ValidationError") {
+        return res.status(STATUS_BAD_REQUEST).send({ message: "Invalid data provided." });
+      }
+
+      // Handle duplicate user error
+      if (err.statusCode === STATUS_UNAUTHORIZE) {
+        return res
+          .status(STATUS_UNAUTHORIZE)
+          .send({ message: "The user already exists." });
+      }
+
+      // Handle unexpected errors
+      res
+        .status(STATUS_DEFAULT)
+        .send({ message: "Internal server error." });
+    });
 };
 
 const login = (req, res) => {
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
 const { STATUS_NOT_FOUND } = require("../utils/constants");
 const { login, createUsers } = require("../controllers/users");
-const auth = require('../middleware/auth');
+const {auth} = require('../middleware/auth');
 
 
 router.use("/items", itemRouter);
 router.post("/signin", login);
-router.post("/signup", createUsers);
+router.post("/signup", auth, createUsers);
 
 router.use("/users", userRouter);
 
