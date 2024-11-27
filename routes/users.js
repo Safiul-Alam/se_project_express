d3f1c3fdf863c5f3562c4bc46e3853d6bc9ec4ee
Safiul alam, Mohammad <alam.buff@gmail.com>
2024-11-27 12:45:23 -0500

    .

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,18 @@
-const express = require('express');
+const express = require("express");
 const { auth } = require("../middleware/auth");
 
 const router = express.Router();
-const { getUsers, createUsers, getUser, getCurrentUser, updateProfile } = require("../controllers/users");
+const {
+  getUsers,
+  createUsers,
+  getUser,
+  getCurrentUser,
+  updateProfile,
+} = require("../controllers/users");
 
-router.get('/users', () => console.log("get users"));
-router.get('/users/:userID', () => console.log("get users by id"));
-router.post('/users', () => console.log("POST users"));
+router.get("/users", () => console.log("get users"));
+router.get("/users/:userID", () => console.log("get users by id"));
+router.post("/users", () => console.log("POST users"));
 
 router.get("/", getUsers);
 router.get("/:userId", getUser);
