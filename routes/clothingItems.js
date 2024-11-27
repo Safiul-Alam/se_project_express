7acf4b69c985b7fa5d5f87d48e39ec06f7bd8425
Safiul alam, Mohammad <alam.buff@gmail.com>
2024-11-27 12:10:17 -0500

    .

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { auth } = require("../middleware/auth");
 
 const router = express.Router();
 
@@ -11,7 +12,7 @@ const {
 } = require("../controllers/clothingItems");
 
 // Create
-router.post("/", createItem);
+router.post("/", auth, createItem);
 
 // READ
 router.get("/", getItems);
@@ -20,12 +21,12 @@ router.get("/", getItems);
 // router.put("/:itemId", updateItem);
 
 // Delete
-router.delete("/:itemId", deleteItem);
+router.delete("/:itemId", auth, deleteItem);
 
 // LIKE ITEM
-router.put("/:itemId/likes", likeItem);
+router.put("/:itemId/likes", auth, likeItem);
 
 // UNLIKE
-router.delete("/:itemId/likes", unlikeItem);
+router.delete("/:itemId/likes", auth, unlikeItem);
 
 module.exports = router;
